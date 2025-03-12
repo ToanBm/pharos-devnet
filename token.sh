37d1102417fbab8a05b1b2c6cd7e7ae6f5c42b1d
toanbm85 <94586777+ToanBm@users.noreply.github.com>
2025-03-12 16:00:27 +0700

    Create token.sh

---
+++ token.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# Logo
+curl -s https://raw.githubusercontent.com/ToanBm/user-info/main/logo.sh | bash
+sleep 3
+
+show() {
+    echo -e "\033[1;35m$1\033[0m"
+}
+
+# Step 1: Install hardhat
+echo "Install Hardhat..."
+npm init -y
+echo "Install dotenv..."
+npm install dotenv
+npm install --save-dev hardhat@2.22.19
+
+# Setup 2: Set Up the Project
+git clone https://github.com/PharosNetwork/examples
+cd examples/token/hardhat/contract
+
+npm install
+
+# Step 3: Create Token.sol contract
+echo "Create ERC20 contract..."
+rm Token.sol
+
+cat <<'EOF' > Token.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Token", "MTK") {
+        _mint(msg.sender, initialSupply);
+    }
+}
+EOF
+
+# Step 4: Update hardhat.config.js with the proper configuration
+echo "Creating new hardhat.config file..."
+rm hardhat.config.ts
+
+cat <<'EOF' > hardhat.config.ts
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    pharos: {
+      url: "https://devnet.dplabs-internal.com",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+EOF
+
+# Step 5: Create .env file for storing private key
+echo "Create .env file..."
+
+read -p "Enter your EVM wallet private key (without 0x): " PRIVATE_KEY
+cat <<EOF > .env
+PRIVATE_KEY=$PRIVATE_KEY
+EOF
+
+# Step 6: Deploying the smart contract
+# Compile the Smart Contract:
+npx hardhat compile
+
+# Test the Smart Contract
+npx hardhat test
+
+# Deploy the Token Contract
+yes | npx hardhat ignition deploy ./ignition/modules/Token.js --network pharos
+
+
+
+
+
+
+
+
+
+
